6805eae7b589ec463ff5babbfb077240b21aa320
jpgomes <jpgomes06@hotmail.com>
2025-11-13 14:09:15 -0300

    ♻️ refactor: renamed createTotpProvider

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ import { CreateAccountController } from './controllers/createAccountController'
 import { UuidProvider } from "./services/uuidProvider";
 import { SecretProvider } from "./services/secretProvider";
 import {QrCodeProvider} from "./services/qrCodeProvider";
-import {CreateTotpProvider} from "./repositories/createTotpProvider";
+import {CreateTotpRepository} from "./repositories/createTotpRepository";
 
 const app = express();
 const PORT = 3000;
 const uuidProvider = new UuidProvider();
 const secretProvider = new SecretProvider();
 const qrCodeProvider = new QrCodeProvider();
-const createTotpProvider = new CreateTotpProvider();
+const createTotpProvider = new CreateTotpRepository();
 
 const createTotpService = new CreateTotpService(uuidProvider, secretProvider, qrCodeProvider, createTotpProvider);
 const createAccountController = new CreateAccountController(createTotpService);
--- src/repositories/createTotpRepository.ts
+++ src/repositories/createTotpRepository.ts
@@ -3,7 +3,7 @@ import { TotpAccount } from "../models/totpAccount";
 import { RabbitMQGateway } from '../infra/rabbitmq-gateway';
 const rabbitMQPublisher = new RabbitMQGateway();
 
-export class CreateTotpProvider implements ICreateTotpRepository{
+export class CreateTotpRepository implements ICreateTotpRepository{
     execute(totpAccount: TotpAccount): Promise<boolean | undefined> {
         return rabbitMQPublisher.publish(totpAccount);
     }
